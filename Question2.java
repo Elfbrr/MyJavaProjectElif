a61a49b743dcbf88c7b8cc567c590ce8d07984fb
Elif <elifnurbirer1994@gmail.com>
2022-12-22 15:16:59 -0800

    this is commit

---
+++ Question2.java
@@ -0,0 +1,12 @@
+public class Question2 {
+    public static void main(String[] args) {
+        // 2. write a method that can convert gallons to litters
+        Question2 sc = new Question2();
+        sc.calculation1(30, 0, 3.75); //gallon to litre
+    }
+
+    public void calculation1(int a, double b, double e) {
+        System.out.println(b = a * e);
+
+    }
+}
