4dfce69196fef8d8eb123e8f95d2b3064b1504d3
Elif <elifnurbirer1994@gmail.com>
2022-12-21 23:33:21 -0800

    first commit by Elif

---
+++ Question1.java
@@ -0,0 +1,12 @@
+public class Question1 {
+    public static void main(String[] args) {
+        //1- Write a method that takes an integer minutes, converts it to seconds and returns as long.
+        Question1 sc = new Question1();
+        sc.calculation(1, 0, 60); //minute to second
+
+    }
+    public void calculation(int a, int b, int e) {
+        System.out.println(b = a * e);
+    }
+
+}
